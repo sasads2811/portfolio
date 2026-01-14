c593c26f05facafdf80a716ea370fe62ac63a0ca
Aleksandar <aleksandar.perovic@factoryww.com>
2026-01-14 14:34:31 +0100

    install template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Navbar } from './layout/Navbar';
+import { Hero } from './sections/Hero';
+import { About } from './sections/About';
+import { Projects } from './sections/Projects';
+import { Experience } from './sections/Experience';
+import { Testimonials } from './sections/Testimonials';
+import { Contact } from './sections/Contact';
+import { Footer } from './layout/Footer';
+
 function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        {/* <Contact /> */}
+      </main>
+      <Footer />
     </div>
   );
 }
